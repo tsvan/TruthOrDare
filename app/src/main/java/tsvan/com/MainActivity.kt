6fd8ee888ab750fc5f49a36c09ec34c5d6b6cddf
tsvan <relaks1237@gmail.com>
2019-12-29 18:17:06 +0700

    template

---
+++ app/src/main/java/tsvan/com/MainActivity.kt
@@ -0,0 +1,13 @@
+package tsvan.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//test
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
