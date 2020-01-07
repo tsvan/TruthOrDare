f546c83e05c88515048a47f1c1ab33d910f3ca71
tsvan <relaks1237@gmail.com>
2020-01-07 19:19:04 +0700

    main menu and add players dialog

--- app/src/main/java/tsvan/com/MainActivity.kt
+++ app/src/main/java/tsvan/com/MainActivity.kt
@@ -1,13 +1,47 @@
 package tsvan.com
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//test
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        newGame.setOnClickListener {
+            this.addPlayersAction()
+        }
+    }
+
+    @SuppressLint("InflateParams")
+    private fun addPlayersAction() {
+        val builder = AlertDialog.Builder(this)
+        val inflater = layoutInflater
+        builder.setTitle("Add players")
+        val dialogLayout = inflater.inflate(R.layout.add_players_dialog, null)
+        val editText  = dialogLayout.findViewById<EditText>(R.id.playerName)
+        builder.setView(dialogLayout)
+        builder.setPositiveButton("Add", null)
+        builder.setNegativeButton("Close") { dialogInterface, _ -> dialogInterface.dismiss() }
+        builder.setNeutralButton("Next") { _, _ -> Toast.makeText(applicationContext, "next ", Toast.LENGTH_SHORT).show() }
+
+        val dialog = builder.create()
+
+        dialog.setOnShowListener {
+            val okButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+            okButton.setOnClickListener {
+                //dialog.dismiss()
+                Toast.makeText(applicationContext, "Add - " + editText.text.toString(), Toast.LENGTH_SHORT).show()
+                editText?.text?.clear()
+            }
+        }
+
+        dialog.show()
     }
 }
