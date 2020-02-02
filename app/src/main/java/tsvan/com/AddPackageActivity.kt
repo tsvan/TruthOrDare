ae16c55ba2ea79c360479e96415a99e992311fdd
tsvan <relaks1237@gmail.com>
2020-02-02 17:21:54 +0700

    new layout

---
+++ app/src/main/java/tsvan/com/AddPackageActivity.kt
@@ -0,0 +1,12 @@
+package tsvan.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPackageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_package)
+    }
+}
